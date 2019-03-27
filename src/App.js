9a598ae0f4d499bb4a0de41286f08844beea8de1
nattapatee <nattapat.ek@bcircle.co.th>
2019-03-27 17:46:57 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux'
+import { increment, decrement } from './action'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = ({ message, counter, dispatch }) => (
+  <div className="container">
+    <div className="columns column is-12">
+      <h1>Counter : {counter}</h1><br></br>
+      <h3>message : {message}</h3>
+    </div>
+
+    <div className="buttons">
+      <button
+        onClick={() => dispatch(increment(1))}
+        className="button is-primary"
+      >
+        +1
+      </button>
+      <button onClick={() => dispatch(increment(2))} className="button is-link">
+        +2
+      </button>
+      <button onClick={() => dispatch(increment(3))} className="button is-info">
+        +3
+      </button>
+    </div>
+
+    <div className="buttons">
+      <button
+        onClick={() => dispatch(decrement(1))}
+        className="button is-primary"
+      >
+        -1
+      </button>
+      <button onClick={() => dispatch(decrement(2))} className="button is-link">
+        -2
+      </button>
+      <button onClick={() => dispatch(decrement(3))} className="button is-info">
+        -3
+      </button>
+    </div>
+  </div>
+)
+
+const mapStateToProps = function(state) {
+  return {
+    message: 'This is message from mapStateToProps',
+    counter: state.counters || 0
   }
 }
 
-export default App;
+
+const AppWithConnect = connect(mapStateToProps)(App)
+export default AppWithConnect
+
